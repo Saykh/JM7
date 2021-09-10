daaa4dc7b12a11b916a070098829e6d13fc5ded1
Saykh <edilov_st@mail.ru>
2021-09-11 01:37:35 +0300

    Commit 3

--- src/main/java/com/edilov/springMVC/models/User.java
+++ src/main/java/com/edilov/springMVC/models/User.java
@@ -30,8 +30,7 @@ public class User {
     public User() {
     }
 
-    public User(int id, String name, String lastName, String email) {
-        this.id = id;
+    public User(String name, String lastName, String email) {
         this.name = name;
         this.lastName = lastName;
         this.email = email;
